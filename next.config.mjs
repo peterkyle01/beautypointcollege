98463089127272e45420f425a6248d193560173e
peterkyle01 <kylepeterkoine4@gmail.com>
2024-06-10 21:10:26 +0300

    fix:images not loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import { hostname } from 'os'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nblprebjyfpqphoxnufp.supabase.co',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
